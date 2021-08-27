482f9bb8e6bad57ac2cffb62ef2c73882424bcfd
ivanchubb <ivanchubb@yahoo.com>
2021-08-27 16:22:08 -0400

    update

--- CRYPTO/script.py
+++ CRYPTO/script.py
@@ -2,9 +2,9 @@ import json
 import math
 from Crypto.Util.number import bytes_to_long, getPrime, long_to_bytes
 
-flag = "YazuaCTF{}"
+flag = "YauzaCTF{" #first 22 digits
 flag = bytes_to_long(flag.encode())
-
+print("first digits are: ", flag)
 primes = [8412883, 8889941, 9251479, 9471269, 9503671, 9723401, 10092149, 10389901, 10551241, 10665527, 11099951]
 shadows = [7832917, 8395798, 4599919, 154544, 3430534, 4694683, 123690, 5911445, 7380167, 10597668]
 threshold = 11
@@ -21,8 +21,12 @@ pmax = mul(primes[:threshold])
 print("pmin is ", pmin)
 print("pmax is ", pmax)
 
+#print(long_to_bytes(pmin))
+#print(long_to_bytes(pmax))
+'''
 j = 0
-i = math.floor(pmin/primes[0])+ shadows[0]
+#i = math.floor(pmin/primes[0])+ shadows[0]
+i = pmin
 while j <11:
     if ((i - shadows[j])%primes[j] != 0):
         print("testing ", i)
@@ -33,7 +37,7 @@ while j <11:
     if j==11:
         print(" THE FLAG IS: ", i)
         break
-
+'''
 
 #print(flag)
 #flag = long_to_bytes(flag)
